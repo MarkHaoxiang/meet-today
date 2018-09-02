45a555202691e20baf75ab9a450fb7cf7caf6f10
Mark Li <mark.haoxiang@gmail.com>
2018-09-02 16:02:45 +0800

    Added virtual environment and all dependancies. Operating on python 3.6

--- app/app.py
+++ app/app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/login')
+def account():
+    return render_template("account.html")
+
 if __name__ == '__main__':
     app.run(debug=True)

f5371d7e3755f9b36552e54b3ce2a3853f1f8b3c
Mark Li <mark.haoxiang@gmail.com>
2018-09-08 13:14:09 +0800

    added chat history + chat creation

--- app/app.py
+++ app/app.py
@@ -3,8 +3,8 @@ from flask_socketio import SocketIO, send, emit, join_room, leave_room
 from flask_mysqldb import MySQL
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, PasswordField, SubmitField, validators
+import os
 from passlib.hash import pbkdf2_sha256
-
 
 app = Flask(__name__)
 #Init
@@ -22,7 +22,11 @@ app.config['MYSQL_DB'] = 'meettoday'
 
 @socketio.on('msg')
 def handleMessage(msg,usr,room):
-	emit('msg', (msg,usr), room=room,broadcast=True)
+    path = "rooms/"+room+".txt"
+    f = open(path, "a")
+    f.write(usr + ":::" + msg+"\n")
+    f.close()
+    emit('msg', (msg,usr), room=room,broadcast=True)
 
 @socketio.on('join')
 def on_join(room):
@@ -52,12 +56,15 @@ class UnameForm(FlaskForm):
 #=======================================================SPACER=============================================================#
 
 @app.route('/')
+#Landing Page
 def index():
     return render_template("index.html")
 
-@app.route('/dashboard')
+#Accounts
+@app.route('/dashboard',methods=['GET','POST'])
 def account():
     #Check if user is logged in
+    url = ""
     try:
         session['logged_in']
     except:
@@ -70,25 +77,80 @@ def account():
     cur = mysql.connect.cursor()
     cur.execute('SELECT*FROM users WHERE username=%s',(uname,))
     user = cur.fetchone()
-    cur.close()
     id = user[0]
     email = user[1]
     date = user[4]
+    if request.method=='POST':
+        cur = mysql.connection.cursor()
+        #Create the room
+        data = request.values
+        roomName = data["name"]
+        roomTime = int(data["time"])
+        roomAuthor = session['user']
+        roomLog = roomName + ".txt"
+        cur.execute('SELECT*FROM rooms WHERE room_name=%s',(roomName,))
+        query = cur.fetchone()
+        if query == None:
+            cur.execute('SELECT id FROM users WHERE username=%s',(roomAuthor,))
+            roomAuthorID = cur.fetchone()
+            cur.execute('INSERT INTO rooms(author_id,room_name,text,run_time) VALUES (%s,%s,%s,%s)',(roomAuthorID,roomName,roomLog,roomTime))
+            mysql.connection.commit()
+            cur.execute('SELECT*FROM rooms')
+            query = cur.fetchall();
+            #Create message history
+            f = open("rooms/"+roomLog, "w")
+            f.close()
+            #Delete all rooms over time
+            for i in query:
+                time = i[5]
+                cur.execute('DELETE FROM rooms WHERE start_date < DATE_SUB(NOW(), INTERVAL %s DAY) AND room_id = %s',(time,i[0]))
+                mysql.connection.commit()
+                if cur.rowcount > 0:
+                    path = "rooms/"+i[3]
+                    if os.path.exists(path):
+                        os.remove(path)
+                    else:
+                        print("The file does not exist")
+            cur.close()
+            url = '/chat/'+roomName
+        redirect(url_for('account'))
     return render_template("dashboard.html",uname=uname,id=id,email=email,date=date)
 
+#Easy logout
 @app.route('/logout')
 def logout():
     session.clear()
     flash('Logged Out', 'success')
     return redirect(url_for('entry'))
 
-
+#List of all public chatrooms/a button to join a chatroom
 @app.route('/chat')
 def join():
     return render_template("join.html")
 
+#A chat room
+@app.route('/invalid')
+def invalid():
+    return render_template('invalid.html')
+
 @app.route('/chat/<string:id>',methods=['GET','POST'])
 def chat(id):
+    #Validate if chatroom exists
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT*FROM rooms WHERE room_name=%s',(id,))
+    result = cur.fetchone()
+    if result == None:
+        return redirect(url_for('invalid'))
+    #Get log of txt
+    path = "rooms/"+result[3]
+    messages = [] #The stored messages of the chat room
+    f = open(path, "r")
+    for line in f:
+        cline = line.split(":::")
+        messages.append((cline[0],cline[1]))
+    f.close()
+    cur.close()
+    #Page Forms
     unameForm = UnameForm()
     try:
         session['username']
@@ -113,14 +175,15 @@ def chat(id):
         session['username'] = unameForm.uname.data
         return redirect(url_for('chat',id=id))
     username = session['username']
-    return render_template("chat.html",id=id,username=username,unameForm=unameForm)
+    return render_template("chat.html",id=id,username=username,unameForm=unameForm,messages=messages)
 
+
+#Login and register
 @app.route('/entry',methods=['GET','POST'])
 def entry():
-    #register
     entryForm = EntryForm()
     loginForm = LoginForm()
-    #login
+    #login, must ensure that correct form with loginform.submit2.data
     if loginForm.submit2.data and loginForm.validate_on_submit():
         uname = loginForm.uname.data
         inputpwd = loginForm.pwd.data
@@ -130,6 +193,7 @@ def entry():
         if result != None:
             #get password if user found
             passwordHash = result[3]
+            #verify password
             if pbkdf2_sha256.verify(inputpwd+key,passwordHash):
                 session['logged_in'] = True
                 session['user'] = uname
@@ -138,7 +202,7 @@ def entry():
                 flash('Incorrect password', 'danger')
         else:
             flash('User not found', 'danger')
-
+            #register
     if entryForm.submit1.data and entryForm.validate_on_submit():
         cur = mysql.connection.cursor()
         uname = entryForm.uname.data
@@ -161,4 +225,4 @@ def entry():
 
 if __name__ == '__main__':
     print("Starting")
-    socketio.run(app,debug=True)
+    socketio.run(app)
